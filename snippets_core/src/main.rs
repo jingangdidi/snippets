2a82fa57b07a569b0a150d10911a411d1c50cb85
Song <35002409+jingangdidi@users.noreply.github.com>
2025-10-15 21:59:41 +0800

    Update main.rs
    add -T print all supported tags

--- snippets_core/src/main.rs
+++ snippets_core/src/main.rs
@@ -1,6 +1,7 @@
 use snippets::{
     parse_paras::parse_para,
     snip::Snippets,
+    SnipTag,
     error::MyError,
 };
 
@@ -14,13 +15,18 @@ fn run() -> Result<(), MyError> {
     // parse paras
     let paras = parse_para()?;
 
-    // init SnipType
-    let my_snippets = Snippets::new(&paras.files, paras.model.clone())?;
-
-    // run
-    if paras.summary.is_empty() {
-        my_snippets.get(paras)
+    if paras.show_tags {
+        println!("supported tags: {}", SnipTag::supported_tags());
+        Ok(())
     } else {
-        my_snippets.print_summary(&paras.summary)
+        // init SnipType
+        let my_snippets = Snippets::new(&paras.files, paras.model.clone())?;
+
+        // run
+        if paras.summary.is_empty() {
+            my_snippets.get(paras)
+        } else {
+            my_snippets.print_summary(&paras.summary)
+        }
     }
 }
